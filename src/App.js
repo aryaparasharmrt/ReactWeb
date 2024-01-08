6d05a670c9223816bbba5aef27af73fb9685e460
Arya Parashar <aryaparasharj02@gmail.com>
2024-01-08 12:44:03 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React Webpage</h1>
+        <p>Edit src/App.js and save to reload.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
